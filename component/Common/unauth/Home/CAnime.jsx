56d66d1241d839fa50ff3e91188d776fb5965ba9
Brilliant Makanju <brilliantmakanju4@gmail.com>
2022-12-28 00:26:16 +0100

    Making Changes to the Homepage using black and orange for background and text with white for text color

--- component/Common/unauth/Home/CAnime.jsx
+++ component/Common/unauth/Home/CAnime.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const CAnime = () => {
     return (
         <>
-           <div className='relative hover:scale-[1.03] duration-200 ease-in-out w-[22vh] h-[40vh]  md:w-[27vh] md:h-[40vh] hover:shadow-lg hover:shadow-gray-900' >
+            <div className='relative hover:scale-[1.03] duration-200 ease-in-out w-[22vh] h-[40vh]  md:w-[27vh] md:h-[40vh] hover:shadow-lg hover:shadow-gray-900' >
                 <Image
                     fill
                     alt='Slide'
@@ -24,7 +24,7 @@ const CAnime = () => {
                     src="/projects/stan1.jpg"
                     className="absolute object-fill"
                 />
-                <button className='absolute w-[90%] bottom-3 left-2 p-3 bg-white text-black ' >Watch Now</button>
+                <button className='absolute rounded-full h-[50px] w-[50px] bottom-3 right-1  bg-[orange] text-black ' >{">"}</button>
 
                 {/* Use thise for Authorized user */}
                 <button className='absolute  top-3 right-2 p-2 px-4 font-extrabold text-[20px] rounded-sm bg-[#968d8da2]  text-white '  >+</button>
@@ -37,7 +37,7 @@ const CAnime = () => {
                     src="/projects/movie6.jpg"
                     className="absolute object-fill"
                 />
-                <button className='absolute w-[90%] bottom-3 left-2 p-3 bg-white text-black ' >Watch Now</button>
+                <button className='absolute rounded-full h-[50px] w-[50px] bottom-3 right-1  bg-[orange] text-black ' >{">"}</button>
 
                 {/* Use thise for Authorized user */}
                 <button className='absolute  top-3 right-2 p-2 px-4 font-extrabold text-[20px] rounded-sm bg-[#968d8da2]  text-white '  >+</button>
@@ -50,7 +50,7 @@ const CAnime = () => {
                     src="/projects/movie1.jpg"
                     className="absolute object-fill"
                 />
-                <button className='absolute w-[90%] bottom-3 left-2 p-3 bg-white text-black ' >Watch Now</button>
+                <button className='absolute rounded-full h-[50px] w-[50px] bottom-3 right-1  bg-[orange] text-black ' >{">"}</button>
 
                 {/* Use thise for Authorized user */}
                 <button className='absolute  top-3 right-2 p-2 px-4 font-extrabold text-[20px] rounded-sm bg-[#968d8da2]  text-white '  >+</button>
@@ -63,7 +63,7 @@ const CAnime = () => {
                     src="/projects/banner-5.jpg"
                     className="absolute object-fill"
                 />
-                <button className='absolute w-[90%] bottom-3 left-2 p-3 bg-white text-black ' >Watch Now</button>
+                <button className='absolute rounded-full h-[50px] w-[50px] bottom-3 right-1  bg-[orange] text-black ' >{">"}</button>
 
                 {/* Use thise for Authorized user */}
                 <button className='absolute  top-3 right-2 p-2 px-4 font-extrabold text-[20px] rounded-sm bg-[#968d8da2]  text-white '  >+</button>
@@ -76,7 +76,7 @@ const CAnime = () => {
                     src="/projects/stan1.jpg"
                     className="absolute object-fill"
                 />
-                <button className='absolute w-[90%] bottom-3 left-2 p-3 bg-white text-black ' >Watch Now</button>
+                <button className='absolute rounded-full h-[50px] w-[50px] bottom-3 right-1  bg-[orange] text-black ' >{">"}</button>
 
                 {/* Use thise for Authorized user */}
                 <button className='absolute  top-3 right-2 p-2 px-4 font-extrabold text-[20px] rounded-sm bg-[#968d8da2]  text-white '  >+</button>
@@ -89,7 +89,7 @@ const CAnime = () => {
                     src="/projects/movie6.jpg"
                     className="absolute object-fill"
                 />
-                <button className='absolute w-[90%] bottom-3 left-2 p-3 bg-white text-black ' >Watch Now</button>
+                <button className='absolute rounded-full h-[50px] w-[50px] bottom-3 right-1  bg-[orange] text-black ' >{">"}</button>
 
                 {/* Use thise for Authorized user */}
                 <button className='absolute  top-3 right-2 p-2 px-4 font-extrabold text-[20px] rounded-sm bg-[#968d8da2]  text-white '  >+</button>
@@ -102,7 +102,7 @@ const CAnime = () => {
                     src="/projects/movie1.jpg"
                     className="absolute object-fill"
                 />
-                <button className='absolute w-[90%] bottom-3 left-2 p-3 bg-white text-black ' >Watch Now</button>
+                <button className='absolute rounded-full h-[50px] w-[50px] bottom-3 right-1  bg-[orange] text-black ' >{">"}</button>
 
                 {/* Use thise for Authorized user */}
                 <button className='absolute  top-3 right-2 p-2 px-4 font-extrabold text-[20px] rounded-sm bg-[#968d8da2]  text-white '  >+</button>
@@ -115,7 +115,7 @@ const CAnime = () => {
                     src="/projects/banner-5.jpg"
                     className="absolute object-fill"
                 />
-                <button className='absolute w-[90%] bottom-3 left-2 p-3 bg-white text-black ' >Watch Now</button>
+                <button className='absolute rounded-full h-[50px] w-[50px] bottom-3 right-1  bg-[orange] text-black ' >{">"}</button>
 
                 {/* Use thise for Authorized user */}
                 <button className='absolute  top-3 right-2 p-2 px-4 font-extrabold text-[20px] rounded-sm bg-[#968d8da2]  text-white '  >+</button>
@@ -124,7 +124,7 @@ const CAnime = () => {
             <div className='relative hover:scale-[1.03] rounded-md flex flex-col justify-center items-center bg-gray-900  duration-200 ease-in-out w-[22vh] h-[40vh]  md:w-[27vh]  md:h-[40vh] hover:shadow-lg hover:shadow-gray-900' >
                 <h3 className='text-white font-extrabold  ' >See More {">"} </h3>
             </div>
-            
+
         </>
     )
 }
